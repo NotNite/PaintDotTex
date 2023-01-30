ae7d4b5b4760c03a56b0f1a3056abd5b80e0a670
NotNite <hi@notnite.com>
2023-01-30 16:47:05 -0500

    add writing

--- PaintDotTex/TexFileType.cs
+++ PaintDotTex/TexFileType.cs
@@ -1,9 +1,12 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
+using Lumina.Data;
 using Lumina.Data.Files;
 using Lumina.Data.Parsing.Tex.Buffers;
 using PaintDotNet;
 using PaintDotNet.Rendering;
+using SurfaceExtensions = PaintDotNet.Rendering.SurfaceExtensions;
 
 namespace PaintDotTex;
 
@@ -12,7 +15,7 @@ public sealed class TexFileType : FileType, IFileTypeFactory {
         : base("FFXIV Texture",
             new FileTypeOptions {
                 LoadExtensions = new[] { ".tex" },
-                SaveExtensions = new string[] { }
+                SaveExtensions = new [] { ".tex" }
             }) { }
 
     public static T ByteToType<T>(BinaryReader reader) {
@@ -25,11 +28,24 @@ public sealed class TexFileType : FileType, IFileTypeFactory {
         return theStructure;
     }
 
+    public static byte[] TypeToByte<T>(T structure) {
+        int size = Marshal.SizeOf(structure);
+        byte[] arr = new byte[size];
+
+        IntPtr ptr = Marshal.AllocHGlobal(size);
+        Marshal.StructureToPtr(structure, ptr, true);
+        Marshal.Copy(ptr, arr, 0, size);
+        Marshal.FreeHGlobal(ptr);
+
+        return arr;
+    }
+
     protected override Document OnLoad(Stream input) {
         var br = new BinaryReader(input);
         var header = ByteToType<TexFile.TexHeader>(br);
 
-        var tex = TextureBuffer.FromStream(header, br);
+        var lbr = new LuminaBinaryReader(input);
+        var tex = TextureBuffer.FromStream(header, lbr);
         var texData = tex.Filter(mip: 0, z: 0, format: TexFile.TextureFormat.B8G8R8A8).RawData;
 
         var doc = new Document(header.Width, header.Height);
@@ -40,13 +56,52 @@ public sealed class TexFileType : FileType, IFileTypeFactory {
                 var color = ColorBgra.FromBgra(texData[(y * header.Width + x) * 4 + 0],
                     texData[(y * header.Width + x) * 4 + 1], texData[(y * header.Width + x) * 4 + 2],
                     texData[(y * header.Width + x) * 4 + 3]);
-                surface.SetPoint(x, y, color);
+                surface[x, y] = color;
             }
         }
 
         doc.Layers.Add(Layer.CreateBackgroundLayer(surface, takeOwnership: true));
-
         return doc;
+    }
+
+    protected override void OnSave(Document input, Stream output, SaveConfigToken token, Surface scratchSurface,
+        ProgressEventHandler callback) {
+        scratchSurface.Clear();
+        input.CreateRenderer().Render(SurfaceExtensions.AsRegionPtr(scratchSurface), Point2Int32.Zero);
+
+        unsafe {
+            var texHeader = new TexFile.TexHeader();
+            texHeader.Width = (ushort)scratchSurface.Width;
+            texHeader.Height = (ushort)scratchSurface.Height;
+            texHeader.Format = TexFile.TextureFormat.B8G8R8A8;
+            texHeader.Depth = 1;
+            texHeader.MipLevels = 1;
+
+            *(int*)texHeader.LodOffset = 0;
+            *(int*)(texHeader.LodOffset + 1) = 1;
+            *(int*)(texHeader.LodOffset + 2) = 1;
+
+            *(int*)texHeader.OffsetToSurface = 80;
+
+            var texData = new byte[scratchSurface.Width * scratchSurface.Height * 4 + sizeof(TexFile.TexHeader)];
+            var bw = new BinaryWriter(new MemoryStream(texData));
+
+            var texHeaderBytes = TypeToByte(texHeader);
+            bw.Write(texHeaderBytes);
+
+
+            for (var y = 0; y < scratchSurface.Height; y++) {
+                for (var x = 0; x < scratchSurface.Width; x++) {
+                    var pixel = scratchSurface[x, y];
+                    bw.Write(pixel.B);
+                    bw.Write(pixel.G);
+                    bw.Write(pixel.R);
+                    bw.Write(pixel.A);
+                }
+            }
+
+            output.Write(texData);
+        }
     }
 
     public FileType[] GetFileTypeInstances() {
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # PaintDotTex
 
-.tex file loading in pdn
+.tex file reading/writing in pdn
+
+NOTE: writing only works in B8R8G8A8 format because i'm lazy. also i build this against the paint.net beta so it might break
